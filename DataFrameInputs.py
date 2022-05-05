a62aab55110f5d39b1c5b49ec166c5fb5def370f
edloughrey <99217900+edloughrey@users.noreply.github.com>
2022-05-05 17:06:00 +0100

    Add files via upload
    Added annotations to graphs and changes to how these are plotted

--- DataFrameInputs.py
+++ DataFrameInputs.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 14 19:38:19 2022
+Created on Tue May  3 12:21:14 2022
 
-@author: maxchesters
+@author: edwardloughrey
 """
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import os.path
 
@@ -16,7 +15,6 @@ import os.path
 This piece of code will allow you to enter the csv files attatched or your own csv files
 and create a data frame and then plot this data which can be compared to the plots in the model
 """
-
  
 
 
@@ -36,15 +34,15 @@ while (RepeatFileInput == True):
     else:
         print('your file does not exist, please try again') 
 
-
+    
 dF_covid.columns = ['Date', 'Cases']
 dF_covid.index = dF_covid['Date']
 dF_covid.pop('Date')
        
 
-expand_dF = (input('Would you like to add more columns (Y/N):'))
+expand_dF = (input('Would you like to add more columns (True/False):'))
 
-while (expand_dF == 'Y'):
+while (expand_dF == 'True'):
     column_addition = str(input('please enter a file name(including file path):'))
     file_exist = os.path.exists(column_addition)
     if (file_exist == True):
@@ -57,7 +55,7 @@ while (expand_dF == 'Y'):
         
         dF_covid[column_name] = extra_column[column_name]
         
-        expand_dF = (input('Would you like to add more columns (Y/ N):'))
+        expand_dF = (input('Would you like to add more columns (True/False):'))
     else:
         print('your file does not exist, please try again')
 
@@ -69,19 +67,106 @@ for x in range (0, len(dF_covid.index)):
     
 dF_covid["Time"] = time
 
+print(dF_covid)
 
-plot_check = input('Would you like to create a graph(Y/N)')
-while (plot_check == 'Y'):
-    plot_list = []
-    plot_number = int(input('How many parameters would you like to plot') )
-    for x in range(0, plot_number):
-        plot_list.append(input('Please enter column title: '))
+
+
+
+allColumns = dF_covid.columns.values.tolist()
+columns_noTime = allColumns.pop()
+# print(allColumns, ", ", columns_noTime)
+
+dF_covid.plot(x='Time', y=allColumns)
+plt.ylabel("Population")
+
+
+
+# plot_check = input('Would you like to create a graph(Y/N)')
+# while (plot_check == 'Y'):
+#     plot_list = []
+#     plot_number = int(input('How many parameters would you like to plot') )
+#     for x in range(0, plot_number):
+#         plot_list.append(input('Please enter column title: '))
     
-    dF_covid.plot(x='Time'  , y = plot_list)
-    plt.ylabel('Population')
-    plt.show()
-    plot_check = input('Would you like to create another graph(Y/N)')
+#     dF_covid.plot(x='Time'  , y = plot_list)
+#     plt.ylabel('Population')
+#     plt.show()
+#     plot_check = input('Would you like to create another graph(Y/N)')
     
+    
+    
+
+
+"""
+
+The code that follows (for uk data) will highlight the key dates of rule/policy changes and
+label them appropriately.
+
+"""
+
+
+
+if file_input == 'cases.csv':     # DEPENDS ON THE FILE NAME
+    uk_dates = input(str("Would you like to highlight UK restriction periods? (Y/N): "))
+    while uk_dates != 'Y' and uk_dates != 'N':
+        uk_dates = input(str("ERROR: Would you like to highlight UK restriction periods? (Y/N): "))
+    
+
+
+    # section applies if user wants to see key dates
+if uk_dates == 'Y':
+       
+        # Making coloured sections
+    plt.axvspan(0, 94, facecolor='purple', alpha=0.2)      # First lockdown
+    plt.axvspan(94, 218, facecolor='yellow', alpha=0.2)      # Pubs open
+    plt.axvspan(218, 245, facecolor='purple', alpha=0.2)   # 2nd national lockdown (with schools still open)
+    plt.axvspan(245, 280, facecolor='red', alpha=0.3)      # Tier system introduced
+    plt.axvspan(280, 341, facecolor='purple', alpha=0.2)   # 3rd national lockdown
+    plt.axvspan(341, 375, facecolor='red', alpha=0.3)      # Schools return
+    plt.axvspan(375, 473, facecolor='yellow', alpha=0.2)     # Pubs open
+
+
+    # If data for tests is included, the graph is greatly apmplified. So the labelling is
+    # different for each version of the line graph
+    
+    if "Vaccinations" in allColumns:
+        plt.text(0, 15000, "1st Lockdown", fontdict=None, fontsize='x-small')
+        plt.text(94, 30000, "Restrictions Eased", fontdict=None, fontsize='x-small')
+        plt.text(218, 75000, "2nd Lockdown", fontdict=None, fontsize='x-small')
+        plt.text(245, 115000, "Tiers Introduced", fontdict=None, fontsize='x-small')
+        plt.text(280, 157500, "3rd Lockdown", fontdict=None, fontsize='x-small')
+        plt.text(341, 187500, "Schools Return", fontdict=None, fontsize='x-small')
+        plt.text(375, 475000, "Restrictions Eased", fontdict=None, fontsize='x-small')
+        plt.text(473, 425000, "All Restrictions Lifted", fontdict=None, fontsize='x-small')
+    
+    
+    # Labelling sections for when 'Tests' column is involved
+    # Would be better if this linked to actual file name
+    elif "Tests" in allColumns:
+        plt.text(0, 140000, "1st Lockdown", fontdict=None, fontsize='x-small')
+        plt.text(94, 285000, "Restrictions Eased", fontdict=None, fontsize='x-small')
+        plt.text(218, 750000, "2nd Lockdown", fontdict=None, fontsize='x-small')
+        plt.text(245, 1000000, "Tiers Introduced", fontdict=None, fontsize='x-small')
+        plt.text(280, 1575000, "3rd Lockdown", fontdict=None, fontsize='x-small')
+        plt.text(341, 1875000, "Schools Return", fontdict=None, fontsize='x-small')
+        plt.text(375, 275000, "Restrictions Eased", fontdict=None, fontsize='x-small')
+        plt.text(473, 175000, "All Restrictions Lifted", fontdict=None, fontsize='x-small')
+    else:
+        plt.text(0, 10000, "1st Lockdown", fontdict=None, fontsize='x-small')
+        plt.text(94, 18000, "Restrictions Eased", fontdict=None, fontsize='x-small')
+        plt.text(218, 80000, "2nd Lockdown", fontdict=None, fontsize='x-small')
+        plt.text(245, 90000, "Tiers Introduced", fontdict=None, fontsize='x-small')
+        plt.text(280, 60000, "3rd Lockdown", fontdict=None, fontsize='x-small')
+        plt.text(341, 50000, "Schools Return", fontdict=None, fontsize='x-small')
+        plt.text(375, 40000, "Restrictions Eased", fontdict=None, fontsize='x-small')
+        plt.text(473, 8000, "All Restrictions Lifted", fontdict=None, fontsize='x-small')
+
+
+plt.show()
+
+
+
+
 
 
 
