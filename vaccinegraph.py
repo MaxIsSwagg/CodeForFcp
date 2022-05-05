4ac1b78dd2cc20e8af92d0ff2c64885ee34de9ac
edatkinson <98823225+edatkinson@users.noreply.github.com>
2022-05-05 17:54:59 +0100

    Graphs using vaccinations.csv data

--- vaccinegraph.py
+++ vaccinegraph.py
@@ -8,31 +8,30 @@ Created on Tue May  3 13:35:27 2022
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.animation import FuncAnimation
+
+def vaccinationgraph():
+    
+
+    df = pd.read_csv("vaccinations.csv")
+
+    df.plot(x = 'Days ', y = ['total_vaccinations', 'people_vaccinated', 'people_fully_vaccinated'])
+    plt.title("Graph of Covid Vaccinations against time in the UK")
+    
+   
+    plt.show()
+
+def vaccinationskernaldensity():
+     df = pd.read_csv("vaccinations.csv")
+     df['people_vaccinated'].plot.kde()
+     plt.title("Kernal Density graph")
+     
+     plt.show()
+    
 
 
+vaccinationgraph()
+vaccinationskernaldensity()
 
-df = pd.read_csv("vaccinations.csv")
-df.plot(x = 'Days ', y = ['total_vaccinations', 'people_vaccinated', 'people_fully_vaccinated'])
-
-
-
-plt.style.use('seaborn')
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-
-
-def animation(i):
-  vaccinationsdata = pd.read_csv('vaccinations.csv')
-  x = []
-  y = []
-  x = vaccinationsdata[0:i]['Days ']
-  y = vaccinationsdata[0:i]['total_vaccinations']
-  
-  ax.clear()
-  ax.plot(x, y)
-
-
-animation = FuncAnimation(fig, func=animation, interval=1000)
-plt.show()
+'''
+Kernal graph basically allows us to tell how many vaccinations were given out during a shortage of vaccinations which most effectively battles the epidemic.
+'''
